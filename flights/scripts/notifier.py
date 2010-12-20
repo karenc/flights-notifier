e7aab3c3e97cf99710d972f6c57ad8e87f26e5fc
karen chan <cee.wing@gmail.com>
2010-12-20 20:56:46 +0000

    Add support for saving and checking previous status

--- flights/scripts/notifier.py
+++ flights/scripts/notifier.py
@@ -2,6 +2,7 @@
 
 import ConfigParser
 import optparse
+import os
 import smtplib
 import sys
 
@@ -18,13 +19,14 @@ def main():
         parser.print_help()
         sys.exit(1)
 
-    airport = getattr(flights.plugins, args[0], None)
+    airport = args[0]
+    plugin = getattr(flights.plugins, airport, None)
     flight_number = args[1]
-    if not airport:
-        sys.stderr.write('Airport %s plugin is not found.\n' % args[0])
+    if not plugin:
+        sys.stderr.write('Airport %s plugin is not found.\n' % airport)
         parser.print_help()
         sys.exit(1)
-    status = airport.get_flight_info(flight_number)
+    status = plugin.get_flight_info(flight_number)
     if not status:
         return
     status = status['status']
@@ -36,7 +38,19 @@ def main():
         config = ConfigParser.ConfigParser()
         config.read(options.configfile)
 
-    if options.email and config and config.has_section('smtp'):
+    tempdir = None
+    old_status = None
+    if config and config.has_section('flight-notifier'):
+        tempdir = config.get('flight-notifier', 'tempdir')
+        if not os.path.exists(tempdir):
+            os.mkdir(tempdir)
+        elif os.path.exists(os.path.join(tempdir, flight_number)):
+            old_status = open(os.path.join(
+                tempdir, flight_number)).read()
+
+    if (options.email and
+            config and config.has_section('smtp') and
+            status != old_status):
         server = smtplib.SMTP(config.get('smtp', 'server'),
                 config.getint('smtp', 'port'))
         server.ehlo()
@@ -44,13 +58,19 @@ def main():
         server.ehlo()
         server.login(config.get('smtp', 'login'),
                 config.get('smtp', 'password'))
-        server.sendmail(config.get('smtp', 'email'),
-                options.email,
-                'From: %s <%s>\n'
-                'To: %s\n'
-                'Subject: Flight status: %s (%s)\n\n'
-                '---\nSent from flights-notifier\n' % (
-                    config.get('smtp', 'name'),
-                    config.get('smtp', 'email'),
-                    options.email, flight_number, status))
+        subject = 'Flight status: %s %s (%s)' % (
+                airport, flight_number, status)
+        for email in options.email.split(','):
+            server.sendmail(config.get('smtp', 'email'),
+                    email,
+                    'From: %s <%s>\n'
+                    'To: %s\n'
+                    'Subject: %s\n\n%s\n\n'
+                    '---\nSent from flights-notifier\n' % (
+                        config.get('smtp', 'name'),
+                        config.get('smtp', 'email'),
+                        email, subject, subject))
         server.close()
+
+        if tempdir:
+            open(os.path.join(tempdir, flight_number), 'w').write(status)
