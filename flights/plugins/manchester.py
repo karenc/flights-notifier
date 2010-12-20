0914be30f554d17d2d266823e4935bcf3779190a
karen chan <cee.wing@gmail.com>
2010-12-20 19:52:59 +0000

    Add try except for json value error in manchester plugin

--- flights/plugins/manchester.py
+++ flights/plugins/manchester.py
@@ -6,7 +6,10 @@ def get_flight_info(flight_number):
     # {u'status': u'Final Boarding', u'aOrD': u'D', u'schdTime': u'16:40', u'schdDate': u'Monday, 20 December 2010', u'destination': u'Amsterdam', u'flightNumber': u'KL1088', u'terminal': u'T2'}
     url = 'http://www.manchesterairport.co.uk/flightinformation/departures.json'
     content = urllib2.urlopen(url).read()
-    info = json.loads(content)
-    for flight in info['flights']:
-        if flight['flightNumber'].lower() == flight_number.lower():
-            return flight
+    try:
+        info = json.loads(content)
+        for flight in info['flights']:
+            if flight['flightNumber'].lower() == flight_number.lower():
+                return flight
+    except ValueError:
+        return
