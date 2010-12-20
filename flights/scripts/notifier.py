504237cb909575bd336f318b29313820d5ac6a22
karen chan <cee.wing@gmail.com>
2010-12-20 19:53:32 +0000

    Add config parser and option parser to notifier script

--- flights/scripts/notifier.py
+++ flights/scripts/notifier.py
@@ -1,20 +1,56 @@
 #!/usr/bin/env python
 
+import ConfigParser
+import optparse
 import smtplib
 import sys
 
 import flights.plugins
 
 def main():
-    if len(sys.argv) != 3:
-        sys.stderr.write('Usage: %s <airport> <flight_number>\n' % sys.argv[0])
+    parser = optparse.OptionParser('Usage: %prog <airport> <flight_number>')
+    parser.add_option('-e', '--email', dest='email',
+            help='send notification to EMAIL', metavar='EMAIL')
+    parser.add_option('-c', '--config', dest='configfile',
+            help='use configuration file')
+    options, args = parser.parse_args()
+    if len(args) != 2:
+        parser.print_help()
         sys.exit(1)
 
-    airport = getattr(flights.plugins, sys.argv[1], None)
-    flight_number = sys.argv[2]
+    airport = getattr(flights.plugins, args[0], None)
+    flight_number = args[1]
     if not airport:
-        sys.stderr.write('Airport %s plugin is not found.\n' % sys.argv[1])
+        sys.stderr.write('Airport %s plugin is not found.\n' % args[0])
+        parser.print_help()
         sys.exit(1)
-    status = airport.get_flight_info(flight_number)['status']
+    status = airport.get_flight_info(flight_number)
     if not status:
         return
+    status = status['status']
+    if 'Departed' in status:
+        return
+
+    config = None
+    if options.configfile:
+        config = ConfigParser.ConfigParser()
+        config.read(options.configfile)
+
+    if options.email and config and config.has_section('smtp'):
+        server = smtplib.SMTP(config.get('smtp', 'server'),
+                config.getint('smtp', 'port'))
+        server.ehlo()
+        server.starttls()
+        server.ehlo()
+        server.login(config.get('smtp', 'login'),
+                config.get('smtp', 'password'))
+        server.sendmail(config.get('smtp', 'email'),
+                options.email,
+                'From: %s <%s>\n'
+                'To: %s\n'
+                'Subject: Flight status: %s (%s)\n\n'
+                '---\nSent from flights-notifier\n' % (
+                    config.get('smtp', 'name'),
+                    config.get('smtp', 'email'),
+                    options.email, flight_number, status))
+        server.close()
