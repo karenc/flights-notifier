882416115427a76f55ede4ead354efb205459843
Karen Chan <cee.wing@gmail.com>
2010-12-21 07:49:49 +0000

    Rename notifier.py to flights_notifier.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
         packages=find_packages(),
         entry_points={
             'console_scripts': [
-                'flights-notifier = travel.scripts.notifier:main',
+                'flights-notifier = travel.scripts.flights_notifier:main',
                 ],
             },
         )
